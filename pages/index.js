22adfbdaa3dfcec6feb0767a4c8e77226245110a
Addeuz <addeus_lhc@hotmail.com>
2020-06-18 11:05:19 +0200

    Initial commit, next base, eslint

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+// import Link from 'next/link';
+// import fetcher from '../server/utils/fetcher';
+
+// export async function getServerSideProps() {
+//   // const pokemonData = await fetcher('http://localhost:3000/api/pokemon');
+//   // const data = pokemonData.results[Math.floor(Math.random() * 10)];
+//   // return { props: { data } };
+// }
+
+const Index = () => (
+  <div>
+    {/* <Link href="/test">
+      <a>Testingtesting</a>
+    </Link> */}
+    <p>Hello World </p>
+  </div>
+);
+export default Index;
