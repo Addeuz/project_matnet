0ea36d8b2c6a24d38b534dec7a4cb738fadfdc99
Andreas Johansson <andreas.nb.johansson@gmail.com>
2020-09-25 11:24:12 +0200

    Second fix of merge

--- components/Admin/Utils/EngineHeadingData.js
+++ components/Admin/Utils/EngineHeadingData.js
@@ -1,7 +1,10 @@
 import { Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
+<<<<<<< HEAD
 import Link from 'next/link';
 import { camelize } from '../../../utils/stringManipulation';
+=======
+>>>>>>> master
 
 const NoMarginH6 = styled.h6`
   margin-bottom: 0.25rem;
@@ -16,6 +19,7 @@ const SCol = styled(Col)`
   padding-left: 0;
 `;
 
+<<<<<<< HEAD
 const GreenSpan = styled.span`
   /* display: inline-block; */
   /* vertical-align: middle; */
@@ -26,6 +30,7 @@ const RedSpan = styled.span`
   /* vertical-align: middle; */
   color: var(--red);
 `;
+
 
 // Because of the magnitude of data that is displayed in an engine this component is made so save up some space compared to 'HeadingData.js' component
 // props:
@@ -133,3 +138,14 @@ export const UserEditingRight = ({ header, canEdit }) => {
     </Row>
   );
 };
+
+  <Row>
+    <Col xs={5}>
+      <NoMarginH6>{header}</NoMarginH6>
+    </Col>
+    <SCol xs={7}>
+      {children ? <span>{children}</span> : <span>Ingen data</span>}
+    </SCol>
+  </Row>
+);
+export default EngineHeadingData;
